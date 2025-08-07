604456d097349cc9fd6f49f0e1d0626472afb2f4
Arijit Basu <connect.arijit25@gmail.com>
2025-08-07 14:55:53 +0000

    ref,source new model

---
+++ models/STG_PAYMENTS.sql
@@ -0,0 +1,10 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
+
+select 
+* 
+from {{ source('raw_source_data', 'RAW_PAYMENTS') }}
+
---
+++ models/ref.sql
@@ -0,0 +1,9 @@
+with 
+    cus as (select * from {{ ref('STG_CUSTOMERS') }}),
+    ord as (select * from {{ref('STG_ORDERS')}})
+
+select
+    c.*,
+    o.*
+from cus c join ord o
+on c.cus_id=o.CUSTOMER_ID
